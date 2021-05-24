bc28e093737913637c0514e3b01e3938fc5c9e56
sherrywilly <sherrywilson521@gmail.com>
2021-05-24 16:42:17 +0530

    commit

--- moodpro/settings.py
+++ moodpro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(a27-vers!^q+k@et5s6v)(ac$eqk^xd=$=)-nb5fludoapis&
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['18.222.116.106', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
